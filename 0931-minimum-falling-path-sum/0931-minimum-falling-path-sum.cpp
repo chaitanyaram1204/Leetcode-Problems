2940019149af42fcec80c96b7bd0bc19c3ee015b
chaitanyaram1204 <127341000+chaitanyaram1204@users.noreply.github.com>
2024-01-19 17:34:22 +0530

    Time: 11 ms (80.66%), Space: 12.4 MB (5.13%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,9 +1,28 @@
 class Solution {
 public:
+    //Greedy 
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int ans = INT_MAX;
+        if(n==0) return 0;
+        if(n==1) return matrix[0][0];
+        for(int i=1 ; i<n ; i++){
+            for(int j=0 ; j<n ; j++){
+                int m = matrix[i-1][j];
+                if( j-1 >= 0) 
+                    m = min(m , matrix[i-1][j-1]);
+                if( j+1 < n) 
+                    m = min(m , matrix[i-1][j+1]);
+                matrix[i][j] += m;
+                if(i==n-1) ans=min(ans,matrix[i][j]);
+            }
+        }
+        return ans;
+    }
     
     // Space optimization
     
-    int minFallingPathSum(vector<vector<int>>& matrix) {
+    /*int minFallingPathSum(vector<vector<int>>& matrix) {
          int n = matrix.size();
         vector<int>front(n,0),curr(n,0);
         for(int j = 0;j<n;j++)
@@ -29,7 +48,7 @@ public:
             maxi = min(maxi,front[j]);
         }
         return maxi;
-    }
+    }*/
     //Dynamic Programming
     /*int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
