324e96f2e32005ce4af20f921e539bf43df952c9
chaitanyaram1204 <127341000+chaitanyaram1204@users.noreply.github.com>
2024-01-21 14:51:12 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (9.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    int x(vector<int> &nums){
-        int n = nums.size();
-        int prev=nums[0];
-        int prev2=0;
-        for(int i = 0;i<n;i++)
+    int x(vector<int>&nums)
+    {
+        int prev = nums[0];
+        int prev2 = 0;
+        for(int i =1;i<nums.size();i++)
         {
-            int not_pick;
-            int pick=nums[i];
+            int pick = nums[i];
             if(i>1)
-            pick +=prev2;
-            not_pick = prev;
+                pick+=prev2;
+            int not_pick = prev;
             int curr = max(pick,not_pick);
-            prev2=prev;
+            prev2 = prev;
             prev = curr;
         }
         return prev;
