848ad73910b0494fe4fd7f4bf577117458b2581c
chaitanyaram1204 <127341000+chaitanyaram1204@users.noreply.github.com>
2024-01-23 21:06:41 +0530

    Time: 173 ms (35.26%), Space: 62.6 MB (35.27%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-   int maxLength(vector<std::string>& arr) {
+   int maxLength(vector<string>& arr) {
         int result = 0;
-        generateSubsequences(arr, 0, "", result);
+       string curr = "";
+        generateSubsequences(arr, 0, curr, result);
         return result;
     }
 
 private:
-    void generateSubsequences(const std::vector<std::string>& arr, int index, const std::string& current, int& result) {
-        std::unordered_set<char> uniqueChars(current.begin(), current.end());
+    void generateSubsequences(vector<string>& arr, int index, string current, int& result) {
+        unordered_set<char> uniqueChars(current.begin(), current.end());
 
         if (uniqueChars.size() == current.size()) {
-            result = std::max(result, static_cast<int>(current.size()));
+            result = max(result, static_cast<int>(current.size()));
             for (int i = index; i < arr.size(); ++i) {
             generateSubsequences(arr, i + 1, current + arr[i], result);
             }
