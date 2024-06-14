23c694b8b2b8d3414e70e9f11aee9aad8559b3e4
Chaitanya Ram <127341000+chaitanyaram1204@users.noreply.github.com>
2024-06-14 17:49:47 +0530

    Time: 544 ms (5.9%), Space: 127.6 MB (6.3%) - LeetHub

--- 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.cpp
+++ 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.cpp
@@ -1,17 +1,50 @@
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
-        vector<int>freq(*max_element(nums.begin(),nums.end())+nums.size()+1,0);
-        for(int i : nums) freq[i]++;
-        int ans = 0;
-        for(int i = 0;i<freq.size();i++)
-        {
-            if(freq[i] <= 1) continue;
-            freq[i+1] += freq[i] - 1;
-            ans += freq[i]-1;
-        }
-        return ans;
-        
+        sort(nums.begin(), nums.end());
+        int no = 0;
+        multiset<int> st;
+        vector<int> temp;
+        unordered_map<int, int> mp;
 
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1]) {
+                no++;
+                st.insert(nums[i]);
+            }
+            mp[nums[i]]++;
+        }
+
+        int count = 0;
+        if (!st.empty()) count = *st.begin() + 1; // Just to avoid pointing to NULL
+
+        while (no != 0) {
+            if (st.find(count) == st.end() && mp.find(count) == mp.end() && count > *st.begin()) {
+                mp[count]++;
+                temp.push_back(count);
+                no--;
+                if (!st.empty()) // Remove only if not empty
+                    st.erase(st.begin());
+                count++;
+            } else if (st.empty() && mp.find(count) == mp.end()) {
+                temp.push_back(count);
+                no--;
+                count++;
+            } else {
+                count++;
+            }
+        }
+
+        int op = 0;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1]) {
+                auto it = lower_bound(temp.begin(), temp.end(), nums[i]);
+                if (it != temp.end()) {
+                    op += (*it - nums[i]);
+                    temp.erase(it);
+                }
+            }
+        }
+        return op;
     }
 };
