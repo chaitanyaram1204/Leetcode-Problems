1f2f840f68790a4c4f3b42ee12bc466280bb5e2e
Chaitanya Ram <127341000+chaitanyaram1204@users.noreply.github.com>
2024-07-10 10:29:18 +0530

    Time: 985 ms (34.84%), Space: 12 MB (9.61%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,42 +1,52 @@
 class Solution {
 public:
-    bool dfs(int i,int j,int count,vector<vector<char>>& board,string word)
+
+    vector<pair<int,int>>directions = {{0,-1},{-1,0},{0,1},{1,0}};
+    bool isValid(int i,int j,int n,int m,vector<vector<char>>&board,string word,vector<vector<int>>&vis,int ind)
     {
-        if(word.length()==count) return true;
-        
-        if(i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j] != word[count])
-            return false;
-        
-        
-        
-        char temp = board[i][j];                  
-        board[i][j] = ' ';                         
-        
-        
-        bool ans = dfs(i-1,j,count+1,board,word) || 
-                   dfs(i+1,j,count+1,board,word) ||
-                   dfs(i,j-1,count+1,board,word) ||
-                   dfs(i,j+1,count+1,board,word);
-        
-        board[i][j] = temp;                         
+        return (i >= 0 && i < n && j >= 0 && j < m && board[i][j] == word[ind] && vis[i][j] == 0);
+    }
+    bool dfs(int i,int j,int n,int m,vector<vector<char>>& board,string word,vector<vector<int>>&vis,int ind)
+    {
+        if(ind == word.size())
+            return true;
+
+        vis[i][j] = 1;
+        bool ans = false;
+        for(auto dir : directions)
+        {
+            int x = i + dir.first;
+            int y = j + dir.second;
+            if(isValid(x,y,n,m,board,word,vis,ind))
+            {
+               // cout << x << " " << y << endl;
+                ans = ans | dfs(x,y,n,m,board,word,vis,ind+1);
+            } 
+        }
+
+        vis[i][j] = 0;
+
         return ans;
     }
-    
-    bool exist(vector<vector<char>>& board, string word) 
-    {
-        int n = board.size();                   
-        int m = board[0].size();             
-        for(int i=0;i<n;i++)
+    bool exist(vector<vector<char>>& board, string word) {
+        int n = board.size();
+        int m = board[0].size();
+
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        bool ans = false;
+        for(int i = 0; i < n ; i++ )
         {
-            for(int j=0;j<m;j++)
+            for( int j = 0 ; j < m ; j++ )
             {
-                if(board[i][j]==word[0] && dfs(i,j,0,board,word))                // whenever we found first letter of word start searching from there
+                if(board[i][j] == word[0])
                 {
-                    return true;
+                    //cout << i << " " << j << endl;
+                    ans = ans | dfs(i,j,n,m,board,word,vis,1);
+
+                    if(ans) return true;
                 }
             }
         }
-        
-        return false;
+        return ans;
     }
 };
