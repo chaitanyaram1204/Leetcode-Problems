e97e37454021f063afbd010731021029a0030f77
Chaitanya Ram <127341000+chaitanyaram1204@users.noreply.github.com>
2024-07-17 19:00:41 +0530

    Time: 13 ms (74.05%), Space: 27.2 MB (21.35%) - LeetHub

---
+++ 1207-delete-nodes-and-return-forest/1207-delete-nodes-and-return-forest.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+        vector<TreeNode*> forest;
+        unordered_set<int> to_delete_set(to_delete.begin(), to_delete.end());
+        
+        dfs(root, true, to_delete_set, forest);
+        return forest;
+    }
+    
+private:
+    TreeNode* dfs(TreeNode* node, bool is_root, unordered_set<int>& to_delete_set, vector<TreeNode*>& forest) {
+        if (!node) return nullptr;
+        
+        bool should_delete = to_delete_set.count(node->val);
+        
+        if (is_root && !should_delete) {
+            forest.push_back(node);
+        }
+        
+        node->left = dfs(node->left, should_delete, to_delete_set, forest);
+        node->right = dfs(node->right, should_delete, to_delete_set, forest);
+        
+        return should_delete ? nullptr : node;
+    }
+};
